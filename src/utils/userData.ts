f5f3e9231056ab91a454e243099021e135f80498
Caio <caiob1305@gmail.com>
2023-09-25 04:30:56 -0300

    Initial

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Caio Bahia",
+  githubUser: "CaioBahia",
+  linkedinUser: "caiobahia",
+  whatsappNumber: "+5571981857503",
+  emailUser: "caiobahia.dev@gmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
